bd486d4f5c05ff13058a0b22dc6ff7ae0860e37c
Danish Munir <danishmunir231@gmail.com>
2022-06-07 23:39:38 +0500

    Acceleration is completed

--- xyzMachineOrientation/ViewController.swift
+++ xyzMachineOrientation/ViewController.swift
@@ -6,14 +6,43 @@
 //
 
 import UIKit
+import CoreMotion
 
 class ViewController: UIViewController {
+    @IBOutlet weak var xLbl: UILabel!
+    @IBOutlet weak var yLbl: UILabel!
+    @IBOutlet weak var zLbL: UILabel!
+    
+    var motionManager: CMMotionManager!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
-    } 
+        motionManager = CMMotionManager()
+        
+        motionManager.startAccelerometerUpdates(to: .main, withHandler: updateLbls)
+    }
 
 
+    func updateLbls(data: CMAccelerometerData?, error: Error?)
+    {
+        guard let data = data else {
+            return
+        }
+        print(data)
+        
+        //Formatter for better understanding
+        let formatter = NumberFormatter()
+        formatter.minimumFractionDigits = 1
+        formatter.maximumFractionDigits = 1
+        
+        let x = formatter.string(for: data.acceleration.x)!
+        let y = formatter.string(for: data.acceleration.y)!
+        let z = formatter.string(for: data.acceleration.z)!
+        
+        print(x, y, z)
+        xLbl.text = "x: \(x)"
+        yLbl.text = "y: \(y)"
+        zLbL.text = "z: \(z)"
+    }
 }
 
